2464eb53a90c6529d6aa9a978327d27ec727e0b8
eternity <saigiridhar21@gmail.com>
2018-07-17 06:17:09 +0530

    Update DefaultCodegen.java

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java
@@ -836,7 +836,7 @@ public class DefaultCodegen {
         importMapping.put("BigDecimal", "java.math.BigDecimal");
         importMapping.put("UUID", "java.util.UUID");
         importMapping.put("File", "java.io.File");
-        importMapping.put("Date", "java.util.Date");
+   //     importMapping.put("Date", "java.util.Date");
         importMapping.put("Timestamp", "java.sql.Timestamp");
         importMapping.put("Map", "java.util.Map");
         importMapping.put("HashMap", "java.util.HashMap");
